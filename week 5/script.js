47a0524304179a1c211b671bb20ce298766bea9a
Roadom <mrolarte@student.apc.edu.ph>
2024-01-12 17:52:34 +0800

    added content related to date get method, constants, operators, math, and comparisons

--- week 5/script.js
+++ week 5/script.js
@@ -12,3 +12,14 @@ lightbox.option({
     'showImageNumberLabel': false,
     'imageFadeDuration': 0
 })
+// Birth stuff
+const birthDate = new Date("September 16, 2003");
+const currentDate = new Date() 
+
+const timeDifference = currentDate - birthDate;
+const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+if (daysDifference > 0) {
+    document.getElementById("days").innerHTML = `I am ${daysDifference} days old 😎`;
+}
+
