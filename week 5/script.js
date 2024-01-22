013d3c3b5ee8d8c2ea50b46de16617911cccc990
Roadom <mrolarte@student.apc.edu.ph>
2024-01-22 14:31:43 +0800

    added the soundboard portion which applies the function, function call, function parameter, DOM method, and DOM elements lessons

--- week 5/script.js
+++ week 5/script.js
@@ -22,4 +22,17 @@ const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 if (daysDifference > 0) {
     document.getElementById("days").innerHTML = `I am ${daysDifference} days old 😎`;
 }
-
+// Soundboard stuff
+function sound(soundName) {
+    const soundPaths = {
+        'anita max win': 'sounds/anita.mp3',
+        'bruh': 'sounds/bruh.mp3',
+        'yowai mo': 'sounds/yowai.mp3',
+        'vine boom': 'sounds/boom.mp3',
+        'huh': 'sounds/huh.mp3',
+    };    
+    const audio = new Audio(soundPaths[soundName]);
+    audio.play();
+    const soundDesc = document.getElementById('sound-desc');
+    soundDesc.textContent = `${soundName}`;
+    }
